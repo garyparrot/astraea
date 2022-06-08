83e3757b4b7a53654fb5761ffebecfb803037431
Zheng-Xian Li <garyparrottt@gmail.com>
2022-06-08 20:34:12 +0800

    enable test

--- app/src/test/java/org/astraea/app/balancer/executor/StraightPlanExecutorTest.java
+++ app/src/test/java/org/astraea/app/balancer/executor/StraightPlanExecutorTest.java
@@ -31,12 +31,10 @@ import org.astraea.app.balancer.log.LogPlacement;
 import org.astraea.app.common.Utils;
 import org.astraea.app.service.RequireBrokerCluster;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class StraightPlanExecutorTest extends RequireBrokerCluster {
 
-  @Disabled
   @Test
   void testRun() throws InterruptedException {
     // arrange
