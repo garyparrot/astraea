d7cce32c5ae78eb92efbf499acc5894e74603012
Zheng-Xian Li <garyparrottt@gmail.com>
2023-03-16 23:47:26 +0800

    Update

--- app/src/main/java/org/astraea/app/DedicateConsumer.java
+++ app/src/main/java/org/astraea/app/DedicateConsumer.java
@@ -41,7 +41,7 @@ public class DedicateConsumer {
                 Map.entry(ConsumerConfig.GROUP_ID_CONFIG, RANDOM_GROUP),
                 Map.entry(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, BytesDeserializer.class),
                 Map.entry(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, BytesDeserializer.class),
-                Map.entry(ConsumerConfig.RECEIVE_BUFFER_CONFIG, 4024000),
+                Map.entry(ConsumerConfig.RECEIVE_BUFFER_CONFIG, 16024000),
                 Map.entry(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, 10485760),
                 Map.entry(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 10485760),
                 Map.entry(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 5048576),
