57f48d7c8c393fcc2d84ef50c9bed7785d8525a2
Zheng-Xian Li <garyparrottt@gmail.com>
2023-03-17 01:08:18 +0800

    Update

--- app/src/main/java/org/astraea/app/DedicateConsumer.java
+++ app/src/main/java/org/astraea/app/DedicateConsumer.java
@@ -40,10 +40,10 @@ public class DedicateConsumer {
             Map.entry(ConsumerConfig.GROUP_ID_CONFIG, RANDOM_GROUP),
             Map.entry(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, BytesDeserializer.class),
             Map.entry(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, BytesDeserializer.class),
-            Map.entry(ConsumerConfig.RECEIVE_BUFFER_CONFIG, 16024000),
-            Map.entry(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, 73400320),
-            Map.entry(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 73400320),
-            Map.entry(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 73400320 / 2),
+            Map.entry(ConsumerConfig.RECEIVE_BUFFER_CONFIG, 800000),
+            Map.entry(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, 10400320),
+            Map.entry(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 10400320),
+            Map.entry(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 10400320 / 2),
             Map.entry(ConsumerConfig.METRICS_SAMPLE_WINDOW_MS_CONFIG, 100),
             Map.entry(ConsumerConfig.CHECK_CRCS_CONFIG, false),
             Map.entry(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 0)));
@@ -90,7 +90,7 @@ public class DedicateConsumer {
           executors.submit(
               () -> {
                 while (!Thread.currentThread().isInterrupted()) {
-                  consumer.poll(Duration.ofSeconds(1));
+                  consumer.poll(Duration.ofMillis(100));
                 }
               });
         });
