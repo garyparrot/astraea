46df9ae90c52ef5c11309e38c1c00bb04d0ce869
Zheng-Xian Li <garyparrottt@gmail.com>
2023-05-08 00:13:29 +0800

    Update

--- app/src/test/java/org/astraea/app/BalancerExperimentTest.java
+++ app/src/test/java/org/astraea/app/BalancerExperimentTest.java
@@ -25,17 +25,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.LongAdder;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import org.astraea.balancer.bench.BalancerBenchmark;
+import org.astraea.common.ByteUtils;
 import org.astraea.common.Configuration;
 import org.astraea.common.Utils;
 import org.astraea.common.admin.Admin;
 import org.astraea.common.admin.Broker;
+import org.astraea.common.admin.BrokerConfigs;
 import org.astraea.common.admin.ClusterInfo;
 import org.astraea.common.admin.NodeInfo;
+import org.astraea.common.admin.TopicPartition;
 import org.astraea.common.balancer.AlgorithmConfig;
 import org.astraea.common.balancer.Balancer;
 import org.astraea.common.balancer.algorithms.GreedyBalancer;
@@ -47,6 +54,8 @@ import org.astraea.common.balancer.algorithms.ResourceBalancer;
 import org.astraea.common.balancer.executor.StraightPlanExecutor;
 import org.astraea.common.cost.CostFunction;
 import org.astraea.common.cost.HasClusterCost;
+import org.astraea.common.cost.HasMoveCost;
+import org.astraea.common.cost.MoveCost;
 import org.astraea.common.cost.NetworkEgressCost;
 import org.astraea.common.cost.NetworkIngressCost;
 import org.astraea.common.cost.NoSufficientMetricsException;
@@ -57,7 +66,10 @@ import org.astraea.common.metrics.ClusterBean;
 import org.astraea.common.metrics.ClusterBeanSerializer;
 import org.astraea.common.metrics.ClusterInfoSerializer;
 import org.astraea.common.metrics.JndiClient;
+import org.astraea.common.metrics.collector.MetricSensor;
 import org.astraea.common.metrics.collector.MetricStore;
+import org.astraea.it.Service;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -79,34 +91,35 @@ public class BalancerExperimentTest {
     try (var admin = Admin.of(realCluster);
         var stream0 = new FileInputStream(fileName0);
         var stream1 = new FileInputStream(fileName1)) {
-      // ClusterInfo clusterInfo0 =
-      //     admin.topicNames(false).thenCompose(admin::clusterInfo).toCompletableFuture().join();
-      System.out.println("Serialize ClusterInfo");
-      ClusterInfo clusterInfo = ClusterInfoSerializer.deserialize(stream0);
-      System.out.println("Serialize ClusterBean");
-      ClusterBean clusterBean = ClusterBeanSerializer.deserialize(stream1);
+      ClusterInfo clusterInfo =
+          admin.topicNames(false).thenCompose(admin::clusterInfo).toCompletableFuture().join();
+
+      // System.out.println("Serialize ClusterInfo");
+      // ClusterInfo clusterInfo = ClusterInfoSerializer.deserialize(stream0);
+      // System.out.println("Serialize ClusterBean");
+      // ClusterBean clusterBean = ClusterBeanSerializer.deserialize(stream1);
       System.out.println("Done!");
 
       Map<HasClusterCost, Double> costMap =
           Map.of(
               new NetworkIngressCost(Configuration.EMPTY), 3.0,
               new NetworkEgressCost(Configuration.EMPTY), 3.0);
+      HasMoveCost moveCost = new ReplicaLeaderCost(
+               Configuration.of(
+                   Map.of(ReplicaLeaderCost.MAX_MIGRATE_LEADER_KEY, "60")));
       var costFunction = HasClusterCost.of(costMap);
 
-      var balancer = new GreedyResourceBalancer3();
+      var balancer = new ResourceBalancer();
       var result =
           BalancerBenchmark.costProfiling()
               .setClusterInfo(clusterInfo)
-              .setClusterBean(clusterBean)
+              .setClusterBean(testUseableClusterBean(costFunction, moveCost))
               .setBalancer(balancer)
               .setExecutionTimeout(Duration.ofSeconds(180))
               .setAlgorithmConfig(
                   AlgorithmConfig.builder()
                       .clusterCost(costFunction)
-                      // .moveCost(
-                      //     new ReplicaLeaderCost(
-                      //         Configuration.of(
-                      //             Map.of(ReplicaLeaderCost.MAX_MIGRATE_LEADER_KEY, "60"))))
+                      .moveCost(moveCost)
                       .build())
               .start()
               .toCompletableFuture()
@@ -258,19 +271,97 @@ public class BalancerExperimentTest {
     }
   }
 
+  ClusterBean testUseableClusterBean(HasClusterCost costFunction, HasMoveCost moveCost) {
+    try (Admin admin = Admin.of(realCluster)) {
+      var clusterInfo =
+          admin.topicNames(false).thenCompose(admin::clusterInfo).toCompletableFuture().join();
+
+      var sensors = MetricSensor.of(List.of(costFunction.metricSensor(), moveCost.metricSensor()));
+
+      try (var metricStore =
+               MetricStore.builder()
+                   .beanExpiration(Duration.ofSeconds(180))
+                   .localReceiver(
+                       () ->
+                           admin
+                               .brokers()
+                               .thenApply(
+                                   (brokers) ->
+                                       brokers.stream()
+                                           .collect(
+                                               Collectors.toUnmodifiableMap(
+                                                   NodeInfo::id,
+                                                   (Broker b) -> JndiClient.of(b.host(), 16926)))))
+                   .sensorsSupplier(() -> Map.of(sensors, (x,y) -> {}))
+                   .build()) {
+        var clusterBean = (ClusterBean) null;
+        var balancer = new GreedyBalancer();
+
+        while (!Thread.currentThread().isInterrupted()) {
+          clusterBean = metricStore.clusterBean();
+
+          System.out.println(
+              clusterBean.all().entrySet().stream()
+                  .collect(
+                      Collectors.toUnmodifiableMap(Map.Entry::getKey, x -> x.getValue().size())));
+          try {
+            Optional<Balancer.Plan> offer =
+                balancer.offer(
+                    AlgorithmConfig.builder()
+                        .clusterInfo(clusterInfo)
+                        .clusterBean(clusterBean)
+                        .clusterCost(costFunction)
+                        .moveCost(moveCost)
+                        .timeout(Duration.ofSeconds(10))
+                        .build());
+            if (offer.isPresent()) {
+              System.out.println("Find one");
+              break;
+            }
+          } catch (NoSufficientMetricsException e) {
+            System.out.println("No Plan, try later: " + e.getMessage());
+            Utils.sleep(Duration.ofSeconds(3));
+          }
+        }
+
+        return clusterBean;
+      }
+    }
+  }
+
   @Disabled
   @Test
-  void testDominantSort() {
-    int[] base = new int[] {1000, 300, 600};
-    List<int[]> collect =
-        IntStream.range(0, 10000)
-            .mapToObj(
-                i ->
-                    new int[] {
-                      ThreadLocalRandom.current().nextInt(0, 10000),
-                      ThreadLocalRandom.current().nextInt(0, 500),
-                      ThreadLocalRandom.current().nextInt(0, 900)
-                    })
-            .collect(Collectors.toUnmodifiableList());
+  void testSerialization() {
+    try (var service = Service.builder()
+        .numberOfBrokers(3)
+        .brokerConfigs(Map.of(BrokerConfigs.LOG_RETENTION_BYTES_CONFIG, "5566"))
+        .build()) {
+      try (var admin = Admin.of(realCluster)) {
+        var clusterInfo = admin.topicNames(false)
+            .thenCompose(admin::clusterInfo)
+            .toCompletableFuture()
+            .join();
+
+        byte[] serialized = ByteUtils.toBytes(clusterInfo);
+        ClusterInfo deserialized = ByteUtils.readClusterInfo(serialized);
+
+        Assertions.assertEquals(clusterInfo.topics().keySet(), deserialized.topics().keySet());
+        for (String topic: clusterInfo.topics().keySet()) {
+          Assertions.assertEquals(
+              clusterInfo.topics().get(topic).topicPartitions(),
+              deserialized.topics().get(topic).topicPartitions()
+          );
+          Assertions.assertEquals(
+              clusterInfo.topics().get(topic).config().raw(),
+              deserialized.topics().get(topic).config().raw());
+        }
+        Assertions.assertEquals(clusterInfo.replicas().size(), deserialized.replicas().size());
+        for (TopicPartition topicPartition : clusterInfo.topicPartitions()) {
+          Assertions.assertEquals(
+              Set.copyOf(clusterInfo.replicas(topicPartition)),
+              Set.copyOf(deserialized.replicas(topicPartition)));
+        }
+      }
+    }
   }
 }
